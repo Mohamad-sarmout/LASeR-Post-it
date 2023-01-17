0df17bd5b993aa1044e72d39f14826a2ad60ad3c
Ahmad-Al-Sayed <ahmad.sayed1414@gmail.com>
2023-01-17 13:27:17 +0200

    Create Navbar

---
+++ src/Component/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Draggable from 'react-draggable'
+import Navbar from './Navbar'
+
+function Home() {
+  return (
+    <div style={{backgroundColor:"#EBEBF0"}}>
+    <Navbar/>
+    </div>
+  )
+}
+
+export default Home
