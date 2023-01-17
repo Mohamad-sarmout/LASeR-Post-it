8d4bd8e15658f7fc56b456d4465cbf297b6c9f84
Ahmad-Al-Sayed <ahmad.sayed1414@gmail.com>
2023-01-17 20:38:40 +0200

    Create Post Page (js & css)

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import Draggable from 'react-draggable'
 import Navbar from './Navbar'
+import Card from './Card/Card'
+import Draggable from 'react-draggable'
 
 function Home() {
   return (
-    <div style={{backgroundColor:"#EBEBF0"}}>
+    <div style={{backgroundColor:"#EBEBF0"}} className='Wrap'>
     <Navbar/>
+    <div className='Main'>
+    <div className='SideBar'></div>
+    <Card/> 
+    </div>
+    
+    
     </div>
   )
 }
